a2dac55c3da3e8eb76bf4d96386ab6abdf9448d6
TopScrech <sergei_saliukov@icloud.com>
2026-06-08 09:12:55 +0200

    Initial Commit

---
+++ Overlazy/OverlazyApp.swift
@@ -0,0 +1,17 @@
+//
+//  OverlazyApp.swift
+//  Overlazy
+//
+//  Created by Sergei Saliukov on 08/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct OverlazyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
