ab53d55a8eb0a0670e983d047eef0a097af7b288
TopScrech <sergei_saliukov@icloud.com>
2026-06-11 17:13:52 +0200

    improved overlay design

--- Overlazy/Views/InputSourceOverlayView.swift
+++ Overlazy/Views/InputSourceOverlayView.swift
@@ -2,15 +2,26 @@ import ScrechKit
 
 struct InputSourceOverlayView: View {
     let inputSource: InputSource
-
+    
     var body: some View {
-        Text(inputSource.displayLanguageCode)
-            .title(.bold)
-            .monospaced()
-            .foregroundStyle(.primary)
-            .lineLimit(1)
-            .minimumScaleFactor(0.7)
-            .frame(width: 96, height: 96)
-            .background(.regularMaterial, in: .rect(cornerRadius: 8))
+        if #available(macOS 26, *) {
+            Text(inputSource.displayLanguageCode)
+                .title(.bold)
+                .monospaced()
+                .foregroundStyle(.primary)
+                .lineLimit(1)
+                .minimumScaleFactor(0.7)
+                .frame(64)
+                .glassEffect(in: .rect(cornerRadius: 8))
+        } else {
+            Text(inputSource.displayLanguageCode)
+                .title(.bold)
+                .monospaced()
+                .foregroundStyle(.primary)
+                .lineLimit(1)
+                .minimumScaleFactor(0.7)
+                .frame(64)
+                .background(.ultraThinMaterial, in: .rect(cornerRadius: 8))
+        }
     }
 }
